0dc1ae79043fd6718294e51b60e22e21d026d4fe
unknown <karolziolkowski252@gmail.com>
2021-03-01 12:16:43 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+#Zad1
+a = 5
+b = 8
+c = 3.5
+d = 10.5
+e = "Karol"
+f = "Ziolkowski"
+g = 1 + 2j
+h = 2 + 1j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+
+#Zad2
+
+x = 5
+y = 10
+dodawanie = x + y
+odejmowanie = x - y
+mnozenie = x * y
+dzielenie = x/y
+print(dodawanie)
+print(odejmowanie)
+print(mnozenie)
+print(dzielenie)
+
+#Zad3
+
+q,w,e,r,t,y = 1,2,3,4,5,6
+q += q
+w -= w
+e *= e
+r /= r
+t **= t
+y %= y
+print(q)
+print(w)
+print(e)
+print(r)
+print(t)
+print(y)
+
+#Zad4
+
+import math
+print(math.e**10)
+
+
+#Zad5
+Imie = "KAROL"
+Nazwisko = "ZIOLKOWSKI"
+print(Imie.capitalize())
+print(Nazwisko.capitalize())
+
+#Zad6
+
+song = "la la la la la la Sing a happy song la la la la la la Smurf the whole day long la la la la la la"
+print(song.count("la"))
+
+
